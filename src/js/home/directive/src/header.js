a595d3cb42a20c8437b14a0e3277331d824f97f4
EdgarAndresSanchezHernandez <eidar_62@hotmail.com>
2018-01-09 22:37:06 -0800

    Nueva versión Jugui-Toño

--- src/js/home/directive/src/header.js
+++ src/js/home/directive/src/header.js
@@ -11,3 +11,31 @@ function myHeader() {
 export default angular.module('directives.myHeader', [])
     .directive('myHeader', myHeader)
     .name;
+
+/* Inicializamos */
+$(document).ready(function () {
+
+    /* Funcion que detecta cuando el scroll baja 100px y le aplica un efecto al navbar */
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() <= 100) {
+
+            $(".navbar").removeClass("dark");
+        } else {
+
+            $(".navbar").addClass("dark");
+        }
+    });
+
+    /* Funcion que detecta cuando cambiamos entre secciones del sitio */
+    $("body").scrollspy({target: ".navbar", offset: 50});
+
+    /* Funcion que aplica un efecto a los links del navbar dependiendo la sección */
+    $(".navbar a").click(function(){
+    
+        $(".navbar").find(".active").addClass("active");
+    
+        $(this).parent().removeClass("active");
+    });
+    
+});
