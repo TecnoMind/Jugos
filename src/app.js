b9b7ff830a5587b7dd7a962e824beae0d9134a1b
EdgarAndresSanchezHernandez <eidar_62@hotmail.com>
2018-01-10 22:19:24 -0800

    Agregamos el menú, problema en el routing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import angular from 'angular';
 import uirouter from '@uirouter/angularjs';
 import home from './js/home';
+import menu from './js/menu';
 import routing from './app.config';
 
-angular.module('app', [uirouter,home])
+angular.module('app', [uirouter,menu])
     .config(routing);
